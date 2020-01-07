fba02fe69204eeb2e9f51fa017ee9372dbef9461
22browner <22browner@da.org>
2020-01-07 15:04:32 -0500

    added Kotlin practice

---
+++ src/Other/Chapters/Chap10Stuff.java
@@ -0,0 +1,44 @@
+package Other.Chapters;
+
+public class Chap10Stuff {
+    private int aNumber;
+    private String aString;
+    private boolean isItTrue;
+
+    public Chap10Stuff(int aNumber, String aString, boolean isItTrue) {
+        this.aNumber = aNumber;
+        this.aString = aString;
+        this.isItTrue = isItTrue;
+    }
+
+    public Chap10Stuff(Chap10Stuff other) {
+        this.aNumber = other.aNumber;
+        this.aString = other.aString;
+        this.isItTrue = other.isItTrue;
+    }
+
+    public Chap10Stuff() {
+        this.aNumber = 0;
+        this.aString = "";
+        this.isItTrue = false;
+    }
+
+    public String toString() {
+        return aNumber + " " + aString + " " + isItTrue;
+    }
+
+    public void setaString(String other) {
+        this.aString = other;
+    }
+
+    
+    public Chap10Stuff(double a) {}
+    public Chap10Stuff(double a, double b) {}
+
+
+    public static void main(String[] args) {
+        Chap10Stuff app1 = new Chap10Stuff(0, 0);
+        Chap10Stuff app2 = new Chap10Stuff(12, "hotdog", true);
+        System.out.println(app2);
+    }
+}
---
+++ src/Other/kotlinpractice/HelloWorld.kt
@@ -0,0 +1,29 @@
+package Other.kotlinpractice
+
+import kotlin.random.Random
+
+val lineDist: ArrayList<Int> = arrayListOf();
+val lineWait: ArrayList<Int> = arrayListOf();
+
+fun smoothArrayGen(arr: ArrayList<Int>, n: Int, rep: Int, max: Int) {
+    for (i in 0..n - 1) {
+        arr.add(Random.nextInt(0, max));
+    }
+
+    for (r in 1..rep) {
+        for (d in 1..n - 2) {
+            arr.set(d, (arr.get(d - 1) + arr.get(d) + arr.get(d + 1)) / 3)
+        }
+    }
+}
+
+
+
+fun main(args: Array<String>) {
+    smoothArrayGen(lineDist,20,1, 15)
+    println("Loading...")
+    for (i in 0..lineDist.size-1) {
+        for (char in 1..lineDist.get(i))
+            print("*")
+        println()
+}
