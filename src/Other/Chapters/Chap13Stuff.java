7813e1e053c3f9776491dc419bbe785ee7017f11
22browner <22browner@da.org>
2020-02-17 09:54:38 -0500

    finished chapter 13 recursion

--- src/Other/Chapters/Chap13Stuff.java
+++ src/Other/Chapters/Chap13Stuff.java
@@ -2,7 +2,6 @@ package Other.Chapters;
 
 public class Chap13Stuff {
     public Chap13Stuff() {
-
     }
 
     public String process(String str) {
@@ -14,8 +13,29 @@ public class Chap13Stuff {
         return str;
     }
 
+    public int power3(int n) {
+        if (n==0)
+            return 1;
+        else {
+            int p = power3(n/2);
+            p *= p;
+            System.out.println("#");
+
+            if (n % 2 == 1) {
+                p *= 3;
+                System.out.println("#");
+            }
+
+
+
+            return p;
+        }
+    }
+
+
     public static void main(String[] args) {
         Chap13Stuff app = new Chap13Stuff();
         System.out.println(app.process("HAVE") + " " + app.process("FUN"));
+        System.out.println(app.power3(15));
     }
 }
